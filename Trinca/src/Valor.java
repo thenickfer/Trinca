c5451be99c69f88d0f2e3bc2b024e2ce9a393ca0
PEDRO KLEIN <davila.pedro.k@gmail.com>
2024-08-21 11:07:11 -0300

    valor novo

--- Trinca/src/Valor.java
+++ Trinca/src/Valor.java
@@ -1,3 +1,8 @@
 public enum Valor {
-    UM, DOIS, TRES, QUATRO, CINCO, SEIS, SETE, OITO, NOVE, DEZ, VALETE, RAINHA, REI;
+    AS(1), DOIS(2), TRES(3), QUATRO(4), CINCO(5), SEIS(6), SETE(7), OITO(8), NOVE(9), DEZ(10), VALETE(11), RAINHA(12), REI(13);
+    private int v;
+    Valor(int v) {
+       this.v=v;
+    }
+    public int getValor(){return v;}
 }
