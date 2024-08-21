082bdef16702520cbd2a12d60c4c452c74a59eee
PEDRO KLEIN <davila.pedro.k@gmail.com>
2024-08-21 15:15:16 -0300

    Menu Pronto

--- Trinca/src/App.java
+++ Trinca/src/App.java
@@ -1,13 +1,36 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Jogo de trinca!");
-
-        Carta c = new Carta(Naipe.COPAS,Valor.CINCO);
-        System.out.println(c.toString());
-        Carta c1 = new Carta(Naipe.ESPADAS,Valor.REI);
-        System.out.println(c1.toString());
-
-        System.out.println(Valor.DAMA.ordinal());
-        System.out.println(Valor.DAMA.getValor());
+        Menu();
+    }
+    public static void Menu(){
+        Scanner sc=new Scanner(System.in);
+        int x=0;
+        while(x!=5){
+            System.out.println("Nova Rodada:");
+            System.out.println("(1) Comprar carta");
+            System.out.println("(2) Trocar carta <n> por <m>");
+            System.out.println("(3) Descartar carta <n>");
+            System.out.println("(4) Verificar vitória");
+            System.out.println("(5) Desistir");
+            x=Integer.parseInt(sc.nextLine());
+            switch (x) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Numero invalido.");
+                    break;
+            }
+        }
     }
 }
