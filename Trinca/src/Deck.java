1e985a11230ec6eaab5e33e4cd3828ac94e551d8
nickfer <nicolas.dcferreira@gmail.com>
2024-08-21 15:12:51 -0300

    deck

--- Trinca/src/Deck.java
+++ Trinca/src/Deck.java
@@ -1,4 +1,22 @@
 public class Deck {
+    private Carta[] cartas = new Carta[54]; 
+    public Deck(){
+        int index = 0;
+        Naipe naipe;
+        for(int i=0;i<4;i++){
+            switch(i){
+                case 0: naipe=Naipe.COPAS;break;
+                case 1: naipe=Naipe.ESPADAS;break;
+                case 2: naipe=Naipe.OUROS;break;
+                case 3: naipe=Naipe.PAUS;break;
+            }
+            for(int j=1;j<=13;j++){
+                cartas[index]=new Carta(naipe, j);
+                index++;
+            }
+        }
+    }
+
     public void insereEmCima(Carta carta){}
     public Carta retiraDeBaixo(){return null;};
     public Carta retiraDeCima(){return null;};
